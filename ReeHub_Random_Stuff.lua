35c9e01e246c70e438f03fe9b9f2c733cb9cd618
botdevXD <54240316+botdevXD@users.noreply.github.com>
2020-10-25 01:44:54 +0100

    Create ReeHub_Random_Stuff.lua

---
+++ ReeHub_Random_Stuff.lua
@@ -0,0 +1,15 @@
+local LIB = {}
+function LIB:FindFunc(v1, v23)
+    for i,v in pairs(getgc(true)) do
+        if type(v) == "function" and islclosure(v) then
+            for i2,v2 in next, debug.getupvalues(v) do
+                if type(v2) == "table" and rawget(v2, v1) then
+                    return v2[v1];
+                else
+                    v23[v1] = nil
+                end;
+            end;
+        end;
+    end;
+end;
+return LIB
