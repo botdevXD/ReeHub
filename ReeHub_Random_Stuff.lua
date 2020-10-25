9000e324942b78de35c4b7a9084bbaa8cca9d797
botdevXD <54240316+botdevXD@users.noreply.github.com>
2020-10-25 02:44:06 +0000

    Update ReeHub_Random_Stuff.lua

--- ReeHub_Random_Stuff.lua
+++ ReeHub_Random_Stuff.lua
@@ -13,16 +13,31 @@ function LIB:FindFunc(v1, v23)
     end;
 end;
 
+--quick fix for Coco Z3
+local FIX_FOR_COCO = (coco and "Coco") or (COCO_LOADED and "Coco")
+
 function LIB:SetFunction(OldFunction, NewFunction)
-    for i,v in pairs(getgc(true)) do
-        if type(v) == "function" and islclosure(v) then
-            for i2,v2 in next, debug.getupvalues(v) do
-                if type(v2) == "table" and rawget(v2, OldFunction) then
-                    v2[OldFunction] = NewFunction
+    if (FIX_FOR_COCO) then
+        for i,v in pairs(getgc(true)) do
+            if type(v) == "function" and islclosure(v) then
+                for i2,v2 in next, debug.getupvalues(v) do
+                    if type(v2) == "table" and rawget(v2, OldFunction) then
+                        v2[OldFunction] = NewFunction
+                    end;
                 end;
             end;
         end;
-    end;
+    else
+        for i,v in pairs(getreg()) do
+            if type(v) == "function" and islclosure(v) then
+                for i2,v2 in next, debug.getupvalues(v) do
+                    if type(v2) == "table" and rawget(v2, OldFunction) then
+                        v2[OldFunction] = NewFunction
+                    end;
+                end;
+            end;
+        end;
+   	end
 end
 
 return LIB
