8f5f933cb2c61bb87cb0fb2221a9677cc88ae24a
botdevXD <54240316+botdevXD@users.noreply.github.com>
2020-10-25 23:47:07 +0000

    Update ReeHub_Random_Stuff.lua

--- ReeHub_Random_Stuff.lua
+++ ReeHub_Random_Stuff.lua
@@ -13,6 +13,20 @@ function LIB:FindFunc(v1, v23)
     end;
 end;
 
+function LIB:FindTable(v1, v23)
+    for i,v in pairs(getgc(true)) do
+        if type(v) == "function" and islclosure(v) then
+            for i2,v2 in next, debug.getupvalues(v) do
+                if type(v2) == "table" and rawget(v2, v1) then
+                    return v2;
+                else
+                    v23[v1] = nil
+                end;
+            end;
+        end;
+    end;
+end;
+
 --quick fix for Coco Z3
 local FIX_FOR_COCO = (coco and "Coco") or (COCO_LOADED and "Coco")
 
