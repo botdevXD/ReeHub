ebd2b708cda8c62c71f61d8a0729edf0b3250e1b
botdevXD <54240316+botdevXD@users.noreply.github.com>
2020-10-25 01:56:55 +0100

    Update ReeHub_Random_Stuff.lua

--- ReeHub_Random_Stuff.lua
+++ ReeHub_Random_Stuff.lua
@@ -12,4 +12,17 @@ function LIB:FindFunc(v1, v23)
         end;
     end;
 end;
+
+function LIB:SetFunction(OldFunction, NewFunction)
+    for i,v in pairs(getreg()) do
+        if type(v) == "function" and islclosure(v) then
+            for i2,v2 in next, debug.getupvalues(v) do
+                if type(v2) == "table" and rawget(v2, OldFunction) then
+                    v2[OldFunction] = NewFunction
+                end;
+            end;
+        end;
+    end;
+end
+
 return LIB
