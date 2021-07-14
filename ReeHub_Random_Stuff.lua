3a81ebee97ce33aed021ad91534225bed1fbb50f
botdevXD <54240316+botdevXD@users.noreply.github.com>
2021-07-14 16:43:34 +0100

    Update ReeHub_Random_Stuff.lua

--- ReeHub_Random_Stuff.lua
+++ ReeHub_Random_Stuff.lua
@@ -4,7 +4,7 @@ local FIX_FOR_COCO = (coco and "Coco") or (COCO_LOADED and "Coco")
 
 function LIB:FindFunc(v1, v23)
 	if FIX_FOR_COCO then
-	    for i,v in pairs(getreg()) do
+	    for i,v in pairs(getgc(true)) do
 	        if type(v) == "function" and islclosure(v) then
 	            for i2,v2 in next, debug.getupvalues(v) do
 	                if type(v2) == "table" and rawget(v2, v1) then
@@ -32,7 +32,7 @@ end;
 
 function LIB:FindTable(v1, v23)
     if FIX_FOR_COCO then
-	    for i,v in pairs(getreg()) do
+	    for i,v in pairs(getgc(true)) do
 	        if type(v) == "function" and islclosure(v) then
 	            for i2,v2 in next, debug.getupvalues(v) do
 	                if type(v2) == "table" and rawget(v2, v1) then
