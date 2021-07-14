5464c3e6cafa30ad53bada7ba4d0f0bd6436f28e
botdevXD <54240316+botdevXD@users.noreply.github.com>
2021-07-14 16:39:03 +0100

    Update ReeHub_Random_Stuff.lua

--- ReeHub_Random_Stuff.lua
+++ ReeHub_Random_Stuff.lua
@@ -1,16 +1,33 @@
 local LIB = {}
+
+local FIX_FOR_COCO = (coco and "Coco") or (COCO_LOADED and "Coco")
+
 function LIB:FindFunc(v1, v23)
-    for i,v in pairs(getgc(true)) do
-        if type(v) == "function" and islclosure(v) then
-            for i2,v2 in next, debug.getupvalues(v) do
-                if type(v2) == "table" and rawget(v2, v1) then
-                    return v2[v1];
-                else
-                    v23[v1] = nil
-                end;
-            end;
-        end;
-    end;
+	if FIX_FOR_COCO then
+	    for i,v in pairs(getreg()) do
+	        if type(v) == "function" and islclosure(v) then
+	            for i2,v2 in next, debug.getupvalues(v) do
+	                if type(v2) == "table" and rawget(v2, v1) then
+	                    return v2[v1];
+	                else
+	                    v23[v1] = nil
+	                end;
+	            end;
+	        end;
+	    end;
+	else
+	    for i,v in pairs(getgc(true)) do
+	        if type(v) == "function" and islclosure(v) then
+	            for i2,v2 in next, debug.getupvalues(v) do
+	                if type(v2) == "table" and rawget(v2, v1) then
+	                    return v2[v1];
+	                else
+	                    v23[v1] = nil
+	                end;
+	            end;
+	        end;
+	    end;
+	end
 end;
 
 function LIB:FindTable(v1, v23)
@@ -26,9 +43,6 @@ function LIB:FindTable(v1, v23)
         end;
     end;
 end;
-
---quick fix for Coco Z3
-local FIX_FOR_COCO = (coco and "Coco") or (COCO_LOADED and "Coco")
 
 function LIB:SetFunction(OldFunction, NewFunction)
     if (FIX_FOR_COCO) then
